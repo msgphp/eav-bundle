4816f4b51b9d8ef8a7b00d765b68e410e5b1bd54
Roland Franssen <franssen.roland@gmail.com>
2017-12-28 13:13:06 +0100

    Automate doctrine config

--- src/DependencyInjection/Extension.php
+++ src/DependencyInjection/Extension.php
@@ -6,20 +6,24 @@ namespace MsgPhp\EavBundle\DependencyInjection;
 
 use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
 use MsgPhp\Domain\Infra\Bundle\ServiceConfigHelper;
+use MsgPhp\Domain\Infra\Uuid\DomainId;
 use MsgPhp\Eav\{AttributeIdInterface, AttributeValueIdInterface};
 use MsgPhp\Eav\Entity\{Attribute, AttributeValue};
 use MsgPhp\Eav\Infra\Doctrine\Repository\AttributeRepository;
+use MsgPhp\Eav\Infra\Doctrine\Type\{AttributeIdType, AttributeValueIdType};
+use Ramsey\Uuid\Uuid;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension as BaseExtension;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader\PhpFileLoader;
 
 /**
  * @author Roland Franssen <franssen.roland@gmail.com>
  */
-final class Extension extends BaseExtension
+final class Extension extends BaseExtension implements PrependExtensionInterface
 {
     public const ALIAS = 'msgphp_eav';
 
@@ -35,16 +39,10 @@ final class Extension extends BaseExtension
 
     public function load(array $configs, ContainerBuilder $container): void
     {
-        $config = $this->processConfiguration($this->getConfiguration($configs, $container), $configs);
-        $classMapping = $config['class_mapping'];
-
-        if (!$classMapping) {
-            // un-configured bundle
-            return;
-        }
-
         $loader = new PhpFileLoader($container, new FileLocator(dirname(__DIR__).'/Resources/config'));
+        $config = $this->processConfiguration($this->getConfiguration($configs, $container), $configs);
         $bundles = array_flip($container->getParameter('kernel.bundles'));
+        $classMapping = $config['class_mapping'];
 
         ServiceConfigHelper::configureEntityFactory($container, $classMapping, [
             Attribute::class => AttributeIdInterface::class,
@@ -53,6 +51,47 @@ final class Extension extends BaseExtension
 
         if (isset($bundles[DoctrineBundle::class])) {
             $this->prepareDoctrineBundle($config, $loader, $container);
+        }
+    }
+
+    public function prepend(ContainerBuilder $container): void
+    {
+        $config = $this->processConfiguration($this->getConfiguration($configs = $container->getExtensionConfig($this->getAlias()), $container), $configs);
+        $bundles = array_flip($container->getParameter('kernel.bundles'));
+        $classMapping = $config['class_mapping'];
+
+        if (isset($bundles[DoctrineBundle::class])) {
+            $container->prependExtensionConfig('doctrine', [
+                'orm' => [
+                    'resolve_target_entities' => $classMapping,
+                    'mappings' => [
+                        'msgphp_eav' => [
+                            'dir' => '%kernel.project_dir%/vendor/msgphp/eav/Infra/Doctrine/Resources/mapping',
+                            'type' => 'xml',
+                            'prefix' => 'MsgPhp\\Eav\\Entity',
+                            'is_bundle' => false,
+                        ],
+                    ],
+                ],
+            ]);
+
+            if (class_exists(Uuid::class)) {
+                $types = [];
+                if (is_subclass_of($classMapping[AttributeIdInterface::class], DomainId::class)) {
+                    $types[AttributeIdType::NAME] = AttributeIdType::class;
+                }
+                if (is_subclass_of($classMapping[AttributeValueIdInterface::class], DomainId::class)) {
+                    $types[AttributeValueIdType::NAME] = AttributeValueIdType::class;
+                }
+
+                if ($types) {
+                    $container->prependExtensionConfig('doctrine', [
+                        'dbal' => [
+                            'types' => $types,
+                        ],
+                    ]);
+                }
+            }
         }
     }
 
