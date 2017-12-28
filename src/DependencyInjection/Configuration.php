eff799f270164afe635d8fe74a38feafc4969131
Roland Franssen <franssen.roland@gmail.com>
2017-12-28 12:24:40 +0100

    Fixes for a fresh install of the bundle (#3)

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -7,6 +7,7 @@ namespace MsgPhp\EavBundle\DependencyInjection;
 use MsgPhp\Eav\{AttributeIdInterface, AttributeValueIdInterface};
 use MsgPhp\Eav\Entity\{Attribute, AttributeValue};
 use MsgPhp\Eav\Infra\Uuid\{AttributeId, AttributeValueId};
+use Ramsey\Uuid\Uuid;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
@@ -28,6 +29,19 @@ final class Configuration implements ConfigurationInterface
                         ->cannotBeEmpty()
                     ->end()
                     ->validate()
+                        ->always()
+                        ->then(function (array $value) {
+                            if (class_exists(Uuid::class)) {
+                                $value += [
+                                    AttributeIdInterface::class => AttributeId::class,
+                                    AttributeValueIdInterface::class => AttributeValueId::class,
+                                ];
+                            }
+
+                            return $value;
+                        })
+                    ->end()
+                    ->validate()
                         ->ifTrue(function (array $value) {
                             return !isset($value[Attribute::class]);
                         })
@@ -40,13 +54,16 @@ final class Configuration implements ConfigurationInterface
                         ->thenInvalid(sprintf('Class "%s" must be configured', AttributeValue::class))
                     ->end()
                     ->validate()
-                        ->always()
-                        ->then(function (array $value) {
-                            return $value + [
-                                AttributeIdInterface::class => AttributeId::class,
-                                AttributeValueIdInterface::class => AttributeValueId::class,
-                            ];
+                        ->ifTrue(function (array $value) {
+                            return !isset($value[AttributeIdInterface::class]);
                         })
+                        ->thenInvalid(sprintf('Class "%s" must be configured. Try `composer require ramsey/uuid`', AttributeIdInterface::class))
+                    ->end()
+                    ->validate()
+                        ->ifTrue(function (array $value) {
+                            return !isset($value[AttributeValueIdInterface::class]);
+                        })
+                        ->thenInvalid(sprintf('Class "%s" must be configured. Try `composer require ramsey/uuid`', AttributeValueIdInterface::class))
                     ->end()
                 ->end()
             ->end()
